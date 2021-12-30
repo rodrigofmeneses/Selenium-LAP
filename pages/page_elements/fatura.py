0df214bdd0389bb59548cf39509bf71bef245aa1
Rodrigo Meneses <rodrigo.menesesufc@gmail.com>
2021-12-30 10:12:36 -0300

    correcao na digitacao de inteiros, dias trabalhados

--- pages/page_elements/fatura.py
+++ pages/page_elements/fatura.py
@@ -46,14 +46,22 @@ class TypeAgent():
         if self._locator_value_equal_data_value(locator, valor):
             # não há nada para digitar
             return
-        # Se o valor tem uma casa decimal, deve digitar de maneira diferente,
-        # com um 0 extra, pois 3.20 se torna 0.32
-        if len(str(valor).split('.')[-1]) == 1:
+        # Para a digitação deve atentar-se as casas decimais
+        if isinstance(valor, int):
+        # Caso seja um inteiro, deve adicionar 2 casas decimais
+            if valor < 0:
+                self._digitar_com_modificador_final(elemento, valor, '00-')
+            else:
+                self._digitar_com_modificador_final(elemento, valor, '00')
+        elif len(str(valor).split('.')[-1]) == 1:
+        # Já se o valor tem uma casa decimal, 
+        # é necessário um 0 extra, pois 3.20 se torna 0.32
             if valor < 0:
                 self._digitar_com_modificador_final(elemento, valor, '0-')
             else:
                 self._digitar_com_modificador_final(elemento, valor, '0')
         else:
+        # Aqui pode-se digitar normalmente
             if valor < 0:
                 self._digitar_com_modificador_final(elemento, valor, '-')
             else:
@@ -90,9 +98,7 @@ class TypeAgent():
 
 class Funcionario(PageElement, TypeAgent):
     def __init__(self, webdriver, _loc_funcionario, dados=None):
-        """Funcionario contém todas as informações dos terceirizados.
-        
-        """
+        """Funcionario contém todas as informações dos terceirizados."""
         self.webdriver = webdriver
         # Informações
         self.nome = (By.CSS_SELECTOR, _loc_funcionario[1] + ' ' + 'div > span')
